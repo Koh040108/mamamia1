e21f05a5ce1e7ad1863970de103976ed44e7ad6e
Santthos <santthos@student.usm.my>
2025-01-19 21:08:15 +0800

    almost ler gambateh

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -133,7 +133,8 @@ const NavBar = () => {
                                 className="w-8 h-8"
                             />
                             {getCartCount() > 0 && (
-                                <span className="absolute top-0 right-0 bg-red-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+                                <span
+                                    className="absolute top-0 right-0 bg-red-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
                                     {getCartCount()}
                                 </span>
                             )}
